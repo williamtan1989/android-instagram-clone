33b4119c5c0258270c4b16fd90044c35baabdc40
William Tan <tankinyeu@gmail.com>
2024-12-01 15:57:42 +0900

    this is ok

--- authorization/build.gradle.kts
+++ authorization/build.gradle.kts
@@ -4,31 +4,8 @@ plugins {
 
 android {
     namespace = "com.williamtan1989.androidinstagramclone.authorization"
-    compileSdk = 35
 
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
     kotlinOptions {
         jvmTarget = "17"
-    }
-    buildFeatures {
-        compose = true
     }
 }
