a8132dda9c1b40e8269b1f213bd154f2a3812dc3
William Tan <tankinyeu@gmail.com>
2024-12-01 13:16:21 +0900

    let's go with snap gram

--- authorization/src/main/kotlin/com/williamtan1989/androidinstagramclone/home/AuthorizationHomeScreen.kt
+++ authorization/src/main/kotlin/com/williamtan1989/androidinstagramclone/home/AuthorizationHomeScreen.kt
@@ -1,6 +1,7 @@
 package com.williamtan1989.androidinstagramclone.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,7 +18,9 @@ fun AuthorizationHomeScreen(
         Box(
             modifier = Modifier.padding(innerPadding)
         ) {
-            Text("Authorization Home!")
+            Column {
+                Text("SnapGram")
+            }
         }
     }
 }
