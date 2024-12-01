37d39f1b7fb110af101ac4dd4fb0c3af049b8ea5
William Tan <tankinyeu@gmail.com>
2024-12-01 15:48:16 +0900

    this is ok

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,61 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.williamtan1989.androidinstagramclone.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+// libs.versions.tomlに追記
+dependencies {
+//    compileOnly(libs.android.gradlePlugin)
+//    compileOnly(libs.android.tools.common)
+//    compileOnly(libs.compose.gradlePlugin)
+////    compileOnly(libs.firebase.crashlytics.gradlePlugin)
+////    compileOnly(libs.firebase.performance.gradlePlugin)
+//    compileOnly(libs.kotlin.gradlePlugin)
+//    compileOnly(libs.ksp.gradlePlugin)
+////    compileOnly(libs.room.gradlePlugin)
+}
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
+    }
+}
+
+gradlePlugin {
+    plugins {
+//        register("androidLibrary") {
+//            id = "com.williamtan1989.androidinstagramclone.androidlibrary"
+//            implementationClass = "com.williamtan1989.androidinstagramclone.AndroidLibraryPlugin"
+//        }
+    }
+}
